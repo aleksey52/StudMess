76791bd22518a8c4eb73c8d652de4058d53120ee
Aleksey Afanasyev <afanasiev2.aa@edu.spbstu.ru>
2022-02-21 17:20:14 +0300

    Initial commit

---
+++ src/main/java/com/spbstu/StudMess/StudMessApplication.java
@@ -0,0 +1,13 @@
+package com.spbstu.StudMess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudMessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudMessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spbstu/StudMess/StudMessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbstu.StudMess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudMessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
